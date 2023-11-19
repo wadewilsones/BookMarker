969fd9aafa337b3664e9512016739c7945ea230a
wadewilso <uladaharanina@gmail.com>
2023-11-19 11:52:25 -0500

    feat: integration tests pass

---
+++ src/test/java/com/bookmark/api/BookmarkControllerTest.java
@@ -0,0 +1,72 @@
+package com.bookmark.api;
+
+import com.bookmark.models.Bookmark;
+import com.bookmark.models.BookmarkRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+//Integration Test
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+ class BookmarkControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    BookmarkRepository bookmarkRepository;
+
+
+    //Before each test delete all entities
+    @BeforeEach
+    void SetUp(){
+        bookmarkRepository.deleteAllInBatch();
+
+        ArrayList<Bookmark>bookmarks = new ArrayList<Bookmark>();
+        bookmarks.add(new Bookmark(null,"Spring Initializer", "https://start.spring.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null,"Spring Initializer", "https://start.spring.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null,"Spring Initializer", "https://start.spring.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null,"Spring Initializer", "https://start.spring.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null,"Spring Initializer", "https://start.spring.io/", Instant.now()));
+
+        bookmarkRepository.saveAll(bookmarks);
+    }
+
+    @Test
+   public void shouldGetBootmarks() throws Exception{
+        mvc.perform(get("/api/bookmarks"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", CoreMatchers.equalTo(5)))
+                .andExpect(jsonPath("$.totalPages", CoreMatchers.equalTo(1)));
+        ;
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, false"
+    })
+    public void shouldGetBootmarksPage(int pageNo, boolean hasNext) throws Exception{
+        mvc.perform(get("/api/bookmarks"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", CoreMatchers.equalTo(5)))
+                .andExpect(jsonPath("$.totalPages", CoreMatchers.equalTo(pageNo)))
+                .andExpect(jsonPath("$.hasNext", CoreMatchers.equalTo(hasNext)));
+        ;
+    }
+}
